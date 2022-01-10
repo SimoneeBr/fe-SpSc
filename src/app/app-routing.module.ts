00483de911850bb157675520736b5e74f1e7f6d2
SimoneBr <simonesuper555@gmail.com>
2022-01-10 19:12:19 +0100

    created scaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {EntryPageComponent} from "./components/entry-page/entry-page.component";
+import {TweetsComponent} from "./components/tweets/tweets.component";
+import {UsersComponent} from "./components/users/users.component";
+import {PlacesComponent} from "./components/places/places.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: EntryPageComponent},
+  {path: 'tweets', component: TweetsComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'places', component: PlacesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EntryPageComponent } from './components/entry-page/entry-page.component';
+import { UsersComponent } from './components/users/users.component';
+import { TweetsComponent } from './components/tweets/tweets.component';
+import { PlacesComponent } from './components/places/places.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    EntryPageComponent,
+    UsersComponent,
+    TweetsComponent,
+    PlacesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
